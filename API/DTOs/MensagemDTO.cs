5a7cdb51d4990c6d4a731342d55f651976b273f7
junioranheu <junioranheu@gmail.com>
2023-01-01 19:45:45 -0300

    ajustar MensagemDTO

--- API/DTOs/MensagemDTO.cs
+++ API/DTOs/MensagemDTO.cs
@@ -1,23 +1,12 @@
 ﻿using API.Models;
-using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
-using static Biblioteca.Utils;
 
 namespace API.DTOs
 {
-    public class MensagemDTO : _RetornoApiDTO
+    public class MensagemDTO
     {
-        [Key]
-        public int MensagemId { get; set; }
         public string? Texto { get; set; } = null;
-
-        // Fk (De lá pra cá);
         public int? UsuarioId { get; set; }
-        [JsonIgnore]
-        public UsuarioDTO? Usuarios { get; set; }
-
-        public DateTime DataRegistro { get; set; } = HorarioBrasilia();
-        public bool IsAtivo { get; set; } = true;
 
         // Fk (De cá pra lá);
         [JsonIgnore]
